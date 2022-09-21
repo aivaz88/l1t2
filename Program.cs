8a03ef8c4315d544c5db6d1a7a70cf16e35c58de
aivaz88 <aivaz1988@mail.ru>
2022-09-22 00:14:25 +0500

    comment

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2 && num1>num3){
+    Console.WriteLine(num1);
+}
+else if (num2 > num1 && num2>num3){
+    Console.WriteLine(num2);
+}
+else {
+    Console.WriteLine(num3);
+}
